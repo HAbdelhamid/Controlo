113d0fcb60dc91c7a0b58640cd1197a0199135e6
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2022-04-14 13:57:06 +0000

    Add: created a table to put on it a list of added books by users --Add:db file for database connection and to create a database

---
+++ App/back-end/db.php
@@ -0,0 +1,47 @@
+<?php
+
+function createdb(){
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname ="e_book";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+        echo "Connected successfully";
+
+
+    //create database
+    $sql ="CREATE DATABASE IF NOT EXISTS $dbname";
+
+    if(mysqli_query($conn, $sql)){
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        $sql = "
+                        CREATE TABLE IF NOT EXISTS books(
+                            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            book_name VARCHAR (25) NOT NULL,
+                            book_publisher VARCHAR (20),
+                            book_price FLOAT 
+                        );
+        ";
+
+        if(mysqli_query($conn, $sql)){
+            return $conn;
+        }else{
+            echo "Cannot Create table...!";
+        }
+
+    }else{
+        echo "Error while creating database ". mysqli_error($conn);
+    }
+
+   
+}
+?>
